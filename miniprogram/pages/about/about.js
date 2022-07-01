62a03822f9f41e49bccf4afba1828b300d2df0d4
yuanlq <yuanlq@pku.edu.cn>
2022-07-01 18:49:06 +0800

    change to the version for others

---
+++ miniprogram/pages/about/about.js
@@ -0,0 +1,48 @@
+//index.js
+
+Page({
+  data: {
+    screenWidth: 0,
+    screenHeight: 0,
+    imgwidth: 0,
+    imgheight: 0
+  },
+  //转发功能
+  onShareAppMessage: function () {
+    let users = wx.getStorageSync('user');
+    if (res.from === 'button') { }
+    return {
+      path: 'pages/about/about',  // 路径，传递参数到指定页面。
+      success: function (res) { }
+    }
+  },
+  onPullDownRefresh: function () {
+    wx.stopPullDownRefresh()
+  },
+  copyTBL: function (e) {
+  var self = this;
+  wx.setClipboardData({
+    data: 'http://gitee.com/circlelq/miniprogram',//需要复制的内容
+    success: function (res) {
+      // self.setData({copyTip:true}),
+     
+    }
+  })
+  },
+
+
+  // 跳转小程序
+  naviToMini:function(e){
+    wx.navigateToMiniProgram({
+      appId: 'wx0fb7b06a5065be09',
+      // path: 'pages/index/index',
+      envVersion: 'release',
+      success(res) {
+        // 打开成功
+      }
+    })
+   }
+  
+})
+
+
